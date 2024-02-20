cfbf34329df0cf55c71e7481c71fb20e99c79107
Enkhtuya Myagmar <23LP6946@G401-CC0091.local>
2024-02-20 01:02:02 -0800

    fs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const { products, users } = require("./dummy.json");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const fs = require("fs");
+// const { error } = require("console");
 
 app.get("/products", (request, response) => {
   response.type = "application/json";
@@ -10,14 +14,51 @@ app.get("/products", (request, response) => {
 
 app.get("/users", (request, response) => {
   response.type = "application/json";
+  response.status(200);
   response.send({ users });
 });
 
-app.listen(3001, () => {
-  console.log("Server is listening");
+// app.listen(3001, () => {
+//   console.log("Server is listening");
+// });
+// app.get("/users", (request, response) => {
+//   response.type = "application/json";
+//   response.status(200);
+//   response.send({ name });
+// });
+
+// app.post("/users", (request, response) => {
+//   let data = request.body;
+//   response.send("Data Received:" + JSON.stringify(data));
+// });
+// app.listen(3001, () => {
+//   console.log("Example app listening on port 3001!");
+// });
+
+app.post("/add-user", (req, res) => {
+  const newUser = req.body;
+  console.log(newUser);
+
+  fs.readFile("dummy.json", (error, data) => {
+    if (error) {
+      console.log("Error in reading file");
+    } else {
+      const jsonFile = JSON.parse(data.toString());
+      jsonFile.users.push(newUser);
+      fs.writeFile("dummy.json", JSON.stringify(jsonFile), (err) => {
+        if (err) {
+          console.log(err);
+          res.send("error occured");
+        } else {
+          console.log("success");
+          res.send("User added successfully");
+        }
+      });
+    }
+  });
 });
-app.get("/users", (request, response) => {
-  response.type = "application/json";
-  response.status(200);
-  response.send({ name });
+// app.post("/read-new-user", (req, res) => {});
+
+app.listen(3001, () => {
+  console.log("Server is listening at port 3001");
 });
